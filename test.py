355e7435132e378dc7320e99c88d88fd7557f6ac
solsolr <greenchamel@gmail.com>
2025-07-16 00:29:29 +0900

    Modify: print문 제거

--- test.py
+++ test.py
@@ -1,7 +1,1 @@
 print ('github 업로드용 테스트 파일!')
-print ('github 업로드용 테스트 파일!')
-print ('github 업로드용 테스트 파일!')
-print ('github 업로드용 테스트 파일!')
-print ('github 업로드용 테스트 파일!')
-print ('github 업로드용 테스트 파일!')
-print ('github 업로드용 테스트 파일!')
